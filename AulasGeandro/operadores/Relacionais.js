3783763cc4345d35bab847ee3c1017776ca9cdd8
jeansdev1 <jeanv_lima@outlook.com>
2024-06-29 16:10:51 -0300

    delet de pastas duplicadas

--- AulasGeandro/operadores/Relacionais.js
+++
@@ -1,6 +0,0 @@
-x = 5
-y = 5 
-let resposta
-
-resposta = x === y
-console.log(resposta)
--- ListaExercicios/entradaDadosdia1/exerc_2.js
+++
@@ -1,9 +0,0 @@
-const leia = require('readline-sync');
-
-n1 = leia.questionFloat('Digite a nota 1: ')
-n2 = leia.questionFloat('Digite a nota 2: ')
-n3 = leia.questionFloat('Digite a nota 3: ')
-n4 = leia.questionFloat('Digite a nota 4: ')
-
-let result = (n1+n2+n3+n4) /4
-console.log('A media final eh', result.toFixed(1))
